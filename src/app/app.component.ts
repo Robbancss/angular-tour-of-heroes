cec9a6bee7a4eb07fd72f93f321eba7a87d50212
Robbancs <robi.panczel@gmail.com>
2017-07-15 15:47:00 +0200

    Refactored the original AppComponent into two components. Added it to the module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero'
 
-export class Hero {
-  id: number;
-  name: string;
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,21 +9,13 @@ export class Hero {
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor='let hero of heroes'
-          (click)='onSelect(hero)'
+      <li *ngFor="let hero of heroes"
           [class.selected]="hero === selectedHero"
-      >
-        <span class='badge'>{{hero.name}}</span> {{hero.name}}
+          (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
     .selected {
